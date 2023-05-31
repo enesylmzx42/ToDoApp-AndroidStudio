ed1ef067ba98663e4cc84a9063243258ba892086
ENES YILMAZ <117593621+enesylmzx42@users.noreply.github.com>
2023-05-31 10:42:57 +0300

    Update LoginPage.java

--- ToDoApp/app/src/main/java/LoginPage.java
+++ ToDoApp/app/src/main/java/LoginPage.java
@@ -2,12 +2,9 @@ package com.example.todoapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.ContentValues;
 import android.content.Intent;
-import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -16,10 +13,7 @@ import android.widget.Toast;
 import com.example.todoapp.Model.UserModel;
 import com.example.todoapp.Utils.DataBaseHelper;
 
-import java.util.ArrayList;
-import java.util.List;
-
-public class SplashActivity extends AppCompatActivity {
+public class LoginPage extends AppCompatActivity {
 
 
     //Variable definitions
@@ -56,28 +50,18 @@ public class SplashActivity extends AppCompatActivity {
 
                     if(email.equals(user.getEmail()) && password.equals(user.getPassword())){
                        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-                       Toast.makeText(SplashActivity.this, "Giriş Yapıldı. " + user.getPassword(), Toast.LENGTH_SHORT).show();
+                       Toast.makeText(LoginPage.this, "Giriş Yapıldı. ", Toast.LENGTH_SHORT).show();
                        intent.putExtra("userMail", user.getEmail());
                        startActivity(intent);
                        txt_email.setText("");
                        txt_password.setText("");
 
                     }else{
-                        Toast.makeText(SplashActivity.this, "E-Posta veya şifre hatalı!", Toast.LENGTH_SHORT).show();
-
-//                        db.execSQL("INSERT INTO TODO_USER (EMAIL, PASSWORD) VALUES ('"+ email +"','"+ password +"')");
-//                        Cursor b = db.rawQuery("SELECT * FROM TODO_USER WHERE EMAIL=('" + email + "')", null);
-//                        b.moveToNext();
-//                        int id = b.getColumnIndex("ID");
-//                        user.setId(b.getInt(id));
-//                        Toast.makeText(SplashActivity.this, "Yeni Kullanıcı Oluşturuldu!", Toast.LENGTH_SHORT).show();
-//                        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-//                        intent.putExtra("a", user.getId());
-//                        startActivity(intent);
+                        Toast.makeText(LoginPage.this, "E-Posta veya şifre hatalı!", Toast.LENGTH_SHORT).show();
                     }
 
                 }else{
-                    Toast.makeText(SplashActivity.this, "Tüm Alanları Doldurmalısınız!", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(LoginPage.this, "Tüm Alanları Doldurmalısınız!", Toast.LENGTH_SHORT).show();
                 }
             }
         });
